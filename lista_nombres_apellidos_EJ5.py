4ab69b95b0891ab49d8d9537b975efcdd079ec7d
brianomg1 <58641790+brianomg1@users.noreply.github.com>
2023-05-30 09:20:03 -0400

    Ejercio_5
    nombre 3 y apellido 3 para lista

---
+++ lista_nombres_apellidos_EJ5.py
@@ -0,0 +1,12 @@
+#EJ5
+nombres=[]
+apellidos = []
+for i in range(3):
+  N=(input("cual nombres eres:"))
+  nombres.append(N)
+  A=(input("cual apellidos eres:"))
+  apellidos.append(A)
+for i in range(3):
+  
+  print(nombres[i],apellidos[i])
+  
